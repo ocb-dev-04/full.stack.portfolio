65c34306e95c2e423b4afcc079412badf0085ae7
Oscar Chavez <42700726+ocb-dev-04@users.noreply.github.com>
2024-12-06 21:15:39 -0400

    *aa token provider

---
+++ src/back/services/auth/Services.Auth.Application/Providers/TokenProvider.cs
@@ -0,0 +1,100 @@
+﻿using System.Text;
+using System.Security.Claims;
+
+using Shared.Common.Helper.Models;
+using Shared.Common.Helper.Providers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Shared.Common.Helper.ErrorsHandler;
+using Services.Auth.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Services.Auth.Application.Providers;
+
+internal sealed class TokenProvider
+{
+    private static readonly Error _authoritationHeaderNotFound
+        = Error.NotFound("authoritationHeaderNotFound", "The jwt was not found");
+
+    private static readonly Error _invalidToken
+        = Error.NotFound("invalidToken", "The jwt is invalid");
+
+    private static readonly string _securityAlgorithm = SecurityAlgorithms.HmacSha256Signature;
+
+    internal Result<string> BuildJwt(
+        in Credential model,
+        in JwtSettings jwtSettings,
+        in JwtSecurityTokenHandler jwtSecurityTokenHandler)
+    {
+        HashSet<Claim> claims = new()
+                {
+                    new Claim(ClaimTypes.Email, model.Email.Value),
+                    new Claim(ClaimTypes.Sid, model.Id.Value.ToString()),
+                    new Claim(ClaimTypes.Hash, model.PrivateKey.Value),
+                };
+
+#if DEBUG
+        DateTime expirationToken = DateTime.UtcNow.AddDays(1);
+#else
+        DateTime expirationToken = DateTime.UtcNow.AddMinutes(30);
+#endif
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(jwtSettings.IssuerSigningKey));
+        SigningCredentials credentials = new(securityKey, _securityAlgorithm);
+        JwtSecurityToken tokenDescriptor = new(
+                issuer: jwtSettings.ValidIssuer,
+                audience: jwtSettings.ValidAudience,
+                claims: claims,
+                expires: expirationToken,
+                signingCredentials: credentials
+            );
+
+        return jwtSecurityTokenHandler.WriteToken(tokenDescriptor);
+    }
+
+    internal Result<CurrentRequestUser> ReadJwt(
+        in JwtSettings jwtSettings,
+        in JwtSecurityTokenHandler jwtSecurityTokenHandler,
+        in HttpRequestProvider httpRequestProvider)
+    {
+        HttpContext httpContext = httpRequestProvider.GetCurrentHttpContext()!;
+        bool getToken = httpContext.Request.Headers.TryGetValue("Authorization", out StringValues jwt);
+        if (!getToken)
+            return Result.Failure<CurrentRequestUser>(_authoritationHeaderNotFound);
+
+        string token = jwt.ToString().Substring("Bearer ".Length).Trim();
+        if (!jwtSecurityTokenHandler.CanReadToken(token))
+            return Result.Failure<CurrentRequestUser>(_invalidToken);
+
+        JwtSecurityToken jwtToken = jwtSecurityTokenHandler.ReadJwtToken(token);
+
+        TokenValidationParameters validationParameters = ValidationParameters(jwtSettings);
+        ClaimsPrincipal principal = jwtSecurityTokenHandler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+        if (validatedToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(_securityAlgorithm, StringComparison.InvariantCultureIgnoreCase))
+            return Result.Failure<CurrentRequestUser>(_invalidToken);
+
+        IEnumerable<Claim> claims = principal.Claims;
+        string email = claims.FirstOrDefault((Claim s) => s.Type.Equals(ClaimTypes.Email))!.Value;
+        string credentialId = claims.FirstOrDefault((Claim s) => s.Type.Equals(ClaimTypes.Sid))!.Value;
+        string languageCode = claims.FirstOrDefault((Claim s) => s.Type.Equals(ClaimTypes.Country))!.Value;
+        string privateKey = claims.FirstOrDefault((Claim s) => s.Type.Equals(ClaimTypes.Hash))!.Value;
+
+        return new CurrentRequestUser(Guid.Parse(credentialId), email, languageCode, privateKey);
+    }
+
+    private static TokenValidationParameters ValidationParameters(JwtSettings jwtSettings)
+        => new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = jwtSettings.ValidateIssuerSigningKey,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.IssuerSigningKey)),
+
+            RequireExpirationTime = false,
+            ValidateLifetime = false,
+
+            ValidateIssuer = jwtSettings.ValidateIssuer,
+            ValidIssuer = jwtSettings.ValidIssuer,
+
+            ValidateAudience = jwtSettings.ValidateAudience,
+            ValidAudience = jwtSettings.ValidAudience
+        };
+}
