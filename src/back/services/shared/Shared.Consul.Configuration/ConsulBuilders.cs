d7feb6927c2c094e3f4a792b77346ab7fd0e9466
Oscar Chavez <42700726+ocb-dev-04@users.noreply.github.com>
2024-12-07 03:48:41 -0400

    *Add consul services and builders

---
+++ src/back/services/shared/Shared.Consul.Configuration/ConsulBuilders.cs
@@ -0,0 +1,52 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using Shared.Consul.Configuration.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Consul.Configuration;
+
+public static class ConsulBuilders
+{
+    public static void AddServiceRegistry(this WebApplication app)
+    {
+        IOptions<ServiceRegistrationSettings> settings = app.Services.GetRequiredService<IOptions<ServiceRegistrationSettings>>();
+        ArgumentNullException.ThrowIfNull(settings.Value, nameof(settings));
+
+        app.AddServiceWhenStart(settings.Value);
+        app.RemoveServiceWhenDown(settings.Value.Id);
+    }
+
+    private static void AddServiceWhenStart(this WebApplication app, ServiceRegistrationSettings settings)
+    {
+        app.Lifetime.ApplicationStarted.Register(() =>
+        {
+            IConsulClient consulClient = app.Services.GetRequiredService<IConsulClient>();
+            
+            AgentServiceRegistration registration = new AgentServiceRegistration
+            {
+                ID = settings.Id,
+                Name = settings.Name,
+                Address = settings.Address,
+                Port = settings.Port,
+                Check = new AgentServiceCheck
+                {
+                    HTTP = settings.ServiceCheck.HealthEndpoint,
+                    Interval = TimeSpan.FromSeconds(settings.ServiceCheck.IntervalToCheckInSeconds),
+                    Timeout = TimeSpan.FromSeconds(settings.ServiceCheck.TimeoutCheckInSeconds)
+                }
+            };
+
+            consulClient.Agent.ServiceRegister(registration).Wait();
+        });
+    }
+
+    private static void RemoveServiceWhenDown(this WebApplication app, string id)
+    {
+        app.Lifetime.ApplicationStopping.Register(() =>
+        {
+            IConsulClient consulClient = app.Services.GetRequiredService<IConsulClient>();
+            consulClient.Agent.ServiceDeregister(id).Wait();
+        });
+    }
+}
---
+++ src/back/services/shared/Shared.Consul.Configuration/Settings/ServiceRegistrationSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Consul.Configuration.Settings;
+
+public sealed class ServiceRegistrationSettings
+{
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Address { get; set; }
+    [Required]
+    public int Port { get; set; }
+
+    public ServiceCheck ServiceCheck { get; set; }
+}
+
+public sealed class ServiceCheck
+{
+    [Required]
+    public string HealthEndpoint { get; set; }
+    public int IntervalToCheckInSeconds { get; set; } = 10;
+    public int TimeoutCheckInSeconds { get; set; } = 5;
+}
