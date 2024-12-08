0db82fc2c83e995c745bc7d8f8777f6e4e968a3b
Oscar Chavez <42700726+ocb-dev-04@users.noreply.github.com>
2024-12-08 04:39:24 -0400

    Update Builders.cs

--- src/back/gateway/Doctor.Management.Gateway/Extensions/Builders.cs
+++ src/back/gateway/Doctor.Management.Gateway/Extensions/Builders.cs
@@ -6,21 +6,22 @@ public static class Builders
 {
     public static async Task AddDynamicRoutes(this WebApplication app)
     {
-		try
-		{
-            ConsulProxyConfigProvider provider = app.Services.GetRequiredService<ConsulProxyConfigProvider>();
+        app.Logger.LogWarning("--> Waiting a few seconds while services register with consul...");
 
+        try
+        {
             // We wait about 20 seconds for all services to be up
             await Task.Delay(20 * 1000);
 
+            ConsulProxyConfigProvider provider = app.Services.GetRequiredService<ConsulProxyConfigProvider>();
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
             cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(10));
 
             await provider.UpdateRoutesAsync(cancellationTokenSource.Token);
         }
-		catch (Exception ex)
-		{
-            app.Logger.LogError("Some error ocurred: {0}", ex.Message);
-		}
+        catch (Exception ex)
+        {
+            app.Logger.LogError("--> Some error ocurred: {0}", ex.Message);
+        }
     }
 }
